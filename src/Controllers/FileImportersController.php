dcf020d33e4cc063451509d38004d39c3fba753f
Xavier Au <xavier.au@anacreation.com>
2019-03-20 00:02:32 +0800

    update sort

--- src/Controllers/FileImportersController.php
+++ src/Controllers/FileImportersController.php
@@ -61,7 +61,8 @@ class FileImportersController extends Controller
 
         $language = Language::whereCode($request->get('lang_code'))->first();
 
-        $files = sort(File::files($request->get('path')));
+        $files = File::files($request->get('path'));
+        sort($files);
 
         //        $files = File::files(storage_path('app/' . $request->get('path')));
         $page = Page::find($request->get('page_id'));
